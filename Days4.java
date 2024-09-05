8faaa3a7c003cd535a3144e79365ee04bbce441e
Ansar-1111 <ansaransar345df@gmail.com>
2024-09-05 20:31:51 +0800

    Membuat printf

---
+++ Days4.java
@@ -0,0 +1,13 @@
+public class Day4 {
+
+    public static void main(String[] args) {
+        
+        System.out.print("nama Ansar");
+        System.out.print("\nnim D0224317");
+        System.out.print("\nkelas b ");
+        
+        System.out.printf("Nama: %s%nUmur: %d tahun%nTinggi Badan: %d cm%nBerat badan: %d kg%n", "ANSAR", 20, 160, 48);
+        
+    }
+    
+}
