da5a7def82ab0f0d74cec16cf9f00b1192f9b1ce
Ansar-1111 <ansaransar345df@gmail.com>
2024-09-06 21:20:28 +0800

    Membuat komentar

---
+++ Days5.java
@@ -0,0 +1,10 @@
+ package Coding100Days;
+public class Day05 {
+    public static void main(String[] args) {
+        System.out.println("hello word"); /** 
+                                                * pesan singkat
+                                                */
+    
+    // motivasi hari ini
+}
+}
