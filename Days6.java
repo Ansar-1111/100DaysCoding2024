50d143a1b78a819814ef07a0208c7d7f0fe03f46
Ansar-1111 <ansaransar345df@gmail.com>
2024-09-07 20:04:10 +0800

    Deklarasi

---
+++ Days6.java
@@ -0,0 +1,12 @@
+package Coding100Days;
+public class Day06 {
+    public static void main(String[] args) {
+        
+        String nama;       //Deklarasi String
+        int umur;          //Deklarasi Integral
+        double tinggi;     //Deklarasi Double 
+        char inisial;      //Deklarasi karakter
+        
+    }
+}
+ 
