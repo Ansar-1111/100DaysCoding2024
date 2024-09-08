1b4d42dda7f1277eb736eedd0e61a7a7e197bc50
Ansar-1111 <ansaransar345df@gmail.com>
2024-09-08 22:38:21 +0800

    Inisialisasi

---
+++ Days7.java
@@ -0,0 +1,13 @@
+package day7;
+
+public class Day07 {
+  
+    public static void main(String[] args) {
+        
+        String nama = "Ansar"; //Inisialisasi saat Deklarasi String
+        int umur = 20;                   //Inisialisasi saat Deklarasi Integral
+        double tinggi = 159.5;           //Inisialisasi saat Deklarasi Double 
+        char inisial = 'A';              //Inisialisasi saat Deklarasi karakter
+    }
+    
+}
