58d2105533242b19a83f45123147762b36804896
Ansar-1111 <ansaransar345df@gmail.com>
2024-09-09 22:52:21 +0800

    Update variabel

---
+++ Days8.java
@@ -0,0 +1,16 @@
+package day8;
+
+public class Day8 {
+
+    public static void main(String[] args) {
+        //deklarasi dan inisialisasi
+        int angka = 11;
+        System.out.println("nilai angka awal: " + angka);
+        
+        //update variabel
+        angka = 18;
+        System.out.println("nilai setelah update: " + angka);
+        
+    }
+    
+}
